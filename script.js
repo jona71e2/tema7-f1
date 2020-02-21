f9763884e81d75a762042c57b5b082cf1e01d70f
Alexander Obel Tyllesen <alex@laptop-8nvkctvc.kea.local>
2020-02-21 12:18:29 +0100

    billeder

--- script.js
+++ script.js
@@ -1,40 +1,87 @@
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get("id");
-    console.log(id);
+        document.addEventListener("DOMContentLoaded", start);
 
-    let personer = [];
+        let personer = [];
+        const endpoint = "https://spreadsheets.google.com/feeds/list/1TWR1C-JT5gHIJvWFRNAQ7t484Ty6hoRx81ws-9H1UqM/od6/public/values?alt=json";
+        const detalje = document.querySelector("#detalje");
+        //https://docs.google.com/spreadsheets/d/1TWR1C-JT5gHIJvWFRNAQ7t484Ty6hoRx81ws-9H1UqM/edit?fbclid=IwAR3lYX9CcWxwW0OONkhH7JS5Ef-tueHiHzzoPx9hAmka9JG0iff5LhW48Mk#gid=0
+        let filter = "alle";
 
-    const endpoint = "https://spreadsheets.google.com/feeds/list/17Dd7DvkPaFamNUdUKlrFgnH6POvBJXac7qyiS6zNRw0/od6/public/values?alt=json";
+        function start() {
+            console.log("start");
+            hentData();
+        }
 
-    const detalje = document.querySelector("#detalje");
+        async function hentData() {
+            console.log("Data");
+            const response = await fetch(endpoint);
+            console.log(response);
+            personer = await response.json();
+            console.log(personer);
+            visPersoner();
+            addEventlistenersToButtons();
 
-    document.addEventListener("DOMContentLoaded", start);
+        }
 
-    function start() {
-        loadData();
-    }
-    async function loadData() {
-        const response = await fetch(endpoint);
-        personer = await response.json();
-        console.log(personer);
+        function addEventlistenersToButtons() {
+            console.log("Knap Eventlisteners");
+            document.querySelectorAll(".filter").forEach(elm => {
+                elm.addEventListener("click", filtrering);
+            })
+        }
 
-        visPersoner();
-    }
-
-    function visPersoner() {
-        detalje.querySelector("button").addEventListener("click", () => {
-            history.back();
-        });
-        personer.feed.entry.forEach(person => {
-            if (ret.gsx$id.$t == id) {
-                detalje.querySelector(".navn").textContent = ret.gsx$navn.$t;
-                detalje.querySelector(".navn").textContent += " - " + ret.gsx$pris.$t + " dkk,-";
-                detalje.querySelector("img").src = "" + ret.gsx$billede.$t + ".jpg";
-                detalje.querySelector("p:nth-child(2)").textContent = `Kategori: ${ret.gsx$kategori.$t}`;
-                detalje.querySelector("p:nth-child(3)").textContent = `Oprindelse: Retten stammer originalt fra ${ret.gsx$oprindelse.$t}`;
-                detalje.querySelector("p:nth-child(4)").textContent = `Beskrivelse: ${ret.gsx$lang.$t}`;
+        function filtrering() {
+            console.log("Filtrering");
+            filter = this.dataset.kategori;
+            document.querySelectorAll(".filter").forEach(elm => {
+                elm.classList.remove("valgt");
+            })
+            this.classList.add("valgt");
+            visPersoner();
+        }
 
 
-            }
-        });
-    }
+        function visPersoner() {
+            console.log("visPersoner function");
+
+            const destination = document.querySelector("article");
+            const container = document.querySelector("#data-container");
+            const personerTemplate = document.querySelector("template");
+            container.innerHTML = "";
+
+            personer.feed.entry.forEach(person => {
+                if (filter == "alle" || filter == person.gsx$kategori.$t) {
+                    let klon = personerTemplate.cloneNode(true).content;
+                    klon.querySelector("h2").textContent = `${person.gsx$navn.$t}`;
+                    klon.querySelector(".flag").src = "billeder/flag/" + person.gsx$land.$t + ".jpg";
+                    klon.querySelector(".profil-billede").src = "billeder/drivers/" + person.gsx$billede.$t;
+                    klon.querySelector("h3").textContent = `${person.gsx$team.$t}`;
+                    klon.querySelector("article").addEventListener("click", () => visDetalje(person));
+
+                    container.appendChild(klon);
+
+                    //link til indsætte billede
+                    //klon.querySelector("img").src = "images/small/" + ret.gsx$billede.$t + "-sm.jpg";
+
+                }
+
+            })
+        }
+
+        function visDetalje(person) {
+            detalje.classList.remove("hide");
+            detalje.querySelector("button").addEventListener("click", () => detalje.classList.add("skjul"));
+            console.log(person.gsx$navn.$t);
+
+
+            detalje.querySelector(".navn").textContent = person.gsx$navn.$t;
+            detalje.querySelector(".navn").textContent += " - " + person.gsx$pris.$t + " dkk,-";
+            detalje.querySelector("img").src = "billeder/drivers/" + person.gsx$billede.$t;
+            detalje.querySelector("p:nth-child(2)").textContent = `Kategori: ${person.gsx$kategori.$t}`;
+            detalje.querySelector("p:nth-child(3)").textContent = `Oprindelse: Retten stammer originalt fra ${person.gsx$oprindelse.$t}`;
+            detalje.querySelector("p:nth-child(4)").textContent = `Beskrivelse: ${person.gsx$lang.$t}`;
+
+
+
+
+
+        }
